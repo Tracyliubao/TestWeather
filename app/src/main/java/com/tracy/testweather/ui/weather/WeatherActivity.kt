a7122eccd970ca9776d62196b0de4ec20da9ea4d
liubao <1215723995@qq.com>
2020-08-18 17:09:38 +0800

    modify:修改标题栏与界面不匹配的问题

--- app/src/main/java/com/tracy/testweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/tracy/testweather/ui/weather/WeatherActivity.kt
@@ -2,6 +2,7 @@ package com.tracy.testweather.ui.weather
 
 import android.content.Context
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -32,7 +33,7 @@ class WeatherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val decorView = window.decorView
-        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         window.statusBarColor = Color.TRANSPARENT
         setContentView(R.layout.activity_weather)
         if (viewModel.lng.isEmpty()){
